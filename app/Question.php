602180baac34a258f040e92fa4d2eb1e42044905
k1cho <aadnanhadzipasic@gmail.com>
2019-01-12 12:58:48 +0100

    seed users and questions tables

--- app/Question.php
+++ app/Question.php
@@ -9,8 +9,13 @@ use App\User;
 class Question extends Model
 {
     protected $fillable = ['title', 'body'];
-    
+
     public function user() {
         return $this->belongsTo(User::class);
+    }
+    
+    public function setTitleAttribute($value) {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug($value);
     }
 }
