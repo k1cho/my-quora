c66d4a2809237d354ce64873a3e883c5d733b03d
k1cho <aadnanhadzipasic@gmail.com>
2019-01-14 16:29:23 +0100

    created Vote icons for Questions and Answers

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -14,18 +14,33 @@
             </div>
             <hr>
             <div class="card grey">
-                <div class="card-body" style="font-size: 20px;">
-                    {!! $question->body_html !!}
-                    <div class="float-right">
-                        <span class="text-muted">
-                            Created {{ $question->created }}
-                        </span>
-                        <div class="media mt-2">
-                            <a href="{{ $question->user->url }}" class="pr-2">
-                                {!! $question->user->avatar !!}
+                <div class="card-body">
+                    <div class="media" style="font-size: 20px;">
+                        <div class="d-flex flex-column vote-controls">
+                            <a title="This question is useful" class="vote-up on">
+                                <i class="fas fa-caret-up fa-2x"></i>
                             </a>
-                            <div class="media-body mt-1">
-                                <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                            <span class="votes-count">123</span>
+                            <a title="This question is not useful" class="vote-down off"><i class="fas fa-caret-down fa-2x"></i></a>
+                            <a title="Mark as Favorite" class="favorite mt-2 favorited">
+                                <i class="fas fa-star fa-2x"></i>
+                            </a>
+                            <span class="favorites-count">123</span>
+                        </div>
+                        <div class="media-body">
+                            {!! $question->body_html !!}
+                            <div class="float-right">
+                                <span class="text-muted">
+                                    Created {{ $question->created }}
+                                </span>
+                                <div class="media mt-2">
+                                    <a href="{{ $question->user->url }}" class="pr-2">
+                                        {!! $question->user->avatar !!}
+                                    </a>
+                                    <div class="media-body mt-1">
+                                        <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -38,24 +53,42 @@
             <div class="card grey">
                 <div class="card-body">
                     <div class="card-title">
-                        <small><strong>{{ $question->answers_count . " " . str_plural('Answers',
-                                $question->answers_count) }}</strong></small>
+                        <small>
+                            <strong>
+                                {{ $question->answers_count . " " . str_plural('Answers',
+                                $question->answers_count) }}
+                            </strong>
+                        </small>
                     </div>
                     <hr>
                     @forelse ($question->answers as $answer)
                     <div class="card grey">
                         <div class="card-body">
-                            {!! $answer->body_html !!}
-                            <div class="float-right">
-                                <span class="text-muted">
-                                    Answered {{ $answer->created }}
-                                </span>
-                                <div class="media mt-2">
-                                    <a href="{{ $answer->user->url }}" class="pr-2">
-                                        {!! $answer->user->avatar !!}
+                            <div class="media">
+                                <div class="d-flex flex-column vote-controls">
+                                    <a title="This answer is useful" class="vote-up on">
+                                        <i class="fas fa-caret-up fa-2x"></i>
                                     </a>
-                                    <div class="media-body mt-1">
-                                        <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
+                                    <span class="votes-count">123</span>
+                                    <a title="This answer is not useful" class="vote-down off"><i class="fas fa-caret-down fa-2x"></i></a>
+                                    <a title="Accept answer" class="vote-accept mt-2 favorited">
+                                        <i class="fas fa-check fa-2x"></i>
+                                    </a>
+                                </div>
+                                <div class="media-body">
+                                    {!! $answer->body_html !!}
+                                    <div class="float-right">
+                                        <span class="text-muted">
+                                            Answered {{ $answer->created }}
+                                        </span>
+                                        <div class="media mt-2">
+                                            <a href="{{ $answer->user->url }}" class="pr-2">
+                                                {!! $answer->user->avatar !!}
+                                            </a>
+                                            <div class="media-body mt-1">
+                                                <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
